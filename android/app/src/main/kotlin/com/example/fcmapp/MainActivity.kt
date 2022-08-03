ddcb9ef3c1a4d76319198fba4d7b9033763480a6
victor <dvicxy@gmail.com>
2022-08-03 15:16:55 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/fcmapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fcmapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FCM APP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignUp(),
+    );
+  }
+}
+
+class SignUp extends StatefulWidget {
+  SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
